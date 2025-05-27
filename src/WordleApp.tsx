81b54c887dc95b11382bf5fe0739800120e7bc42
Alejandro Sanchez <alejandro.sanchez@biossmann.com>
2025-05-27 12:30:29 -0600

    add dark mode

--- src/WordleApp.tsx
+++ src/WordleApp.tsx
@@ -2,8 +2,8 @@ import { AppRouter } from "./router/AppRouter";
 
 export const WordleApp = () => {
   return (
-    <>
+    <div className="h-dvh bg-white dark:bg-dark text-black dark:text-white">
       <AppRouter />
-    </>
+    </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
 import { BrowserRouter } from "react-router";
 
 import "./styles.css";
 
+import { ThemeProvider } from "./theme";
 import { WordleApp } from "./WordleApp.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <WordleApp />
+      <ThemeProvider>
+        <WordleApp />
+      </ThemeProvider>
     </BrowserRouter>
   </StrictMode>
 );

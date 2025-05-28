2bd337248078668a8634df6ec9575f07dee2dc19
Alejandro Sanchez <alejandro.sanchez@biossmann.com>
2025-05-27 18:08:24 -0600

    add board html design & styles

--- src/wordle/constants/index.ts
+++ src/wordle/constants/index.ts
@@ -1,5 +1,14 @@
 import { SpecialCharacter } from "../enums";
 
+export const BOARD_ROWS: number = 5;
+export const BOARD_COLUMNS: number = 6;
+
+export const BOARD_ROWS_ARRAY = Array.from({ length: BOARD_ROWS }, (_, i) => i);
+export const BOARD_COLUMNS_ARRAY = Array.from(
+  { length: BOARD_COLUMNS },
+  (_, i) => i
+);
+
 export const KEY_BOARD_ARR: string[][] = [
   ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
   ["A", "S", "D", "F", "G", "H", "J", "K", "L", "Ñ"],
--- src/wordle/views/NormalView.tsx
+++ src/wordle/views/NormalView.tsx
@@ -1,9 +1,10 @@
-import { Keyboard } from "../components";
+import { GuessBoard, Keyboard } from "../components";
 
 export const NormalView = () => {
   return (
-    <>
+    <div className="grid gap-4">
+      <GuessBoard />
       <Keyboard />
-    </>
+    </div>
   );
 };
